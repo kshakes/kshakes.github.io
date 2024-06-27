0672b86b1dde6c05d96e9d8622852f87f3f826fc
Shakezk <kyleshakes04@gmail.com>
2024-06-27 20:50:22 +0100

    Added a salary calculator
    I have created this salary calculator to make it easy for my co-workers to calculate how much money they will take home after deductions

---
+++ mts-salary/mts.js
@@ -0,0 +1,28 @@
+var allowance = 1047.50;
+//possible pension pay of 7% ? 
+$(".calculate-salary").click(function(){
+  var rate = $("#hourly-rate-text").val();
+  if ($.trim(rate) === '') {
+    $('#totalPay').text("Hourly Rate Not Given");
+  }else{
+    rate = parseFloat(rate);
+    var fridayOTPay = $("#fridayOT-worked-text").val() * (rate * 1.33);
+    var saturdayOTPay = $("#saturdayOT-worked-text").val() * (rate * 1.50);
+    var sundayOTPay = $("#sundayOT-worked-text").val() * (rate * 2);
+    var extraPay = parseFloat($("#extra-pay-text").val()) || 0;
+
+    var preTaxPay = ((260/12) * (rate) * 9) + fridayOTPay + saturdayOTPay + sundayOTPay + extraPay;
+
+    var tax = (preTaxPay - allowance) * 0.20;
+    var nationalInsurance = (preTaxPay - allowance) * 0.08;
+
+    var postTax = preTaxPay - tax - nationalInsurance;
+
+    const formattedNumber = postTax.toLocaleString('en-GB', {
+        style: 'currency',
+        currency: 'GBP',
+      });
+
+    $('#totalPay').text(formattedNumber); 
+  }  
+})
