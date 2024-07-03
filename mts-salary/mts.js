13765e27e6253e087ef2816050bc213680641622
Kyle <kyleshakes04@gmail.com>
2024-07-03 07:32:54 +0100

    Added NI, Tax and pre tax pay
    Added 3 more elements so the user can see how much they picked up before tax and how much tax and national insurance they pay

--- mts-salary/mts.js
+++ mts-salary/mts.js
@@ -12,17 +12,40 @@ $(".calculate-salary").click(function(){
     var extraPay = parseFloat($("#extra-pay-text").val()) || 0;
 
     var preTaxPay = ((260/12) * (rate) * 9) + fridayOTPay + saturdayOTPay + sundayOTPay + extraPay;
-
+    // const preTaxFormatted = preTaxPay.toLocaleString('en-GB', {
+    //   style: 'currency',
+    //   currency: 'GBP',
+    // });
     var tax = (preTaxPay - allowance) * 0.20;
     var nationalInsurance = (preTaxPay - allowance) * 0.08;
 
     var postTax = preTaxPay - tax - nationalInsurance;
 
-    const formattedNumber = postTax.toLocaleString('en-GB', {
-        style: 'currency',
-        currency: 'GBP',
-      });
     $('.show-salary-container').css('display', 'flex');
-    $('#totalPay').text(formattedNumber + "pm"); 
+    const preTaxPayObj = document.getElementById("preTaxPay");
+    const nIDeductObj = document.getElementById("nIDeduct");
+    const taxDeductObj = document.getElementById("taxDeduct");
+    const totalPayObj = document.getElementById("totalPay");
+    animateValue(preTaxPayObj, 0, preTaxPay, 2000);
+    animateValue(nIDeductObj, 0, nationalInsurance, 2000);
+    animateValue(taxDeductObj, 0, tax, 2000);
+    animateValue(totalPayObj, 0, postTax, 2000);
   }  
 })
+
+function animateValue(obj, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = "£" + Math.round((progress * (end - start) + start) * 100) / 100;
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+$(".backButton").click(function(){
+  $('.show-salary-container').fadeOut("slow"); // make it disappear
+});
