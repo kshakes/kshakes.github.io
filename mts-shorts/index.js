316f979727d55cdb47d85b9bad130beac179c38c
Shakezk <kyleshakes04@gmail.com>
2024-08-06 21:45:52 +0100

    Modified shorting system

--- mts-shorts/index.js
+++ mts-shorts/index.js
@@ -7,7 +7,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-let shorts = [];
+let shorts = [{
+    "Bham West":[],
+    "Worcester":[],
+    "Black Country":[],
+    "Stafford":[],
+    "Shrewsbury":[],
+    "Bham Central":[],
+    "Stoke South":[],
+    "Reddich":[],
+    "Bham North":[],
+    "Burton":[],
+    "Stoke North":[]
+}];
 let numOfShorts = 0;
 let shortsResolved = 0;
 
@@ -28,23 +40,19 @@ app.get("/shorts/new", (req, res) => {
 });
 
 app.post("/shorts", (req, res) => {
-    const dropExists = shorts.some(short => short.drop === req.body.drop);
-
     const id = Date.now().toString();
     const short = {
         id: id,
+        drop: req.body.drop,
         size: req.body.size,
         rating: req.body.rating,
         brand: req.body.brand,
         type: req.body.type,
         quantity: req.body.quantity,
     };
-
-    if (!dropExists){
-        short.drop = req.body.drop;
-    }
-
-    shorts.push(short);
+    shorts[0][req.body.drop].push(short);
+    console.log(shorts);
+    console.log(shorts[0][req.body.drop]);
     numOfShorts++;
     res.redirect("/");
 });
@@ -59,11 +67,10 @@ app.get("/shorts/:id/edit", (req, res) => {
 });
 
 app.post("/shorts/:id/update", (req, res) => {
+    //fix the ability to edit
     const { id } = req.params;
     const {drop, size, rating, brand, type, quantity} = req.body;
     const short = shorts.find(s => s.id === id);
-
-    const oldDrop = short.drop;
 
     short.drop = drop;
     short.size = size;
@@ -72,40 +79,16 @@ app.post("/shorts/:id/update", (req, res) => {
     short.type = type;
     short.quantity = quantity;
 
-    if (oldDrop !== drop){
-        const dropExists = shorts.some(s => s.drop === drop && s.id !== id);
-        if (dropExists) {
-            delete short.drop;
-        }
-    }
-    const oldDropExists = shorts.some(s => s.drop === oldDrop);
-    if (!oldDropExists) {
-        shorts = shorts.map(s => {
-            if (s.drop === oldDrop) {
-                delete s.drop;
-            }
-            return s;
-        });
-    }
     res.redirect("/");
 })
 
 app.post("/shorts/:id/delete", (req, res) => {
+    //fix the ability to delete
     const short = shorts.find(s => s.id === req.params.id);
     console.log(short);
-    const oldDrop = short.drop;
 
     shorts = shorts.filter(s => s.id !== req.params.id);
 
-    const oldDropExists = shorts.some(s => s.drop === oldDrop);
-    if (!oldDropExists) {
-        shorts = shorts.map(s => {
-            if (s.drop === oldDrop) {
-                delete s.drop;
-            }
-            return s;
-        });
-    }
     shortsResolved++;
     res.redirect("/");
 });
