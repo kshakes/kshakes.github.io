c89cd546b6a132989b58d0bfaa3f90ae76b53836
Shakezk <kyleshakes04@gmail.com>
2024-08-20 17:59:27 +0100

    Added preview

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 $(document).ready(function() {
     $("h3").mouseenter(function(evt) {
-        if ($(this).hasClass("firstProject") || $(this).hasClass("secondProject") || $(this).hasClass("thirdProject")) {
-            $(this).css("color", "green");
+        var bgImage = "";
+
+        if ($(this).hasClass("firstProject")) {
+            bgImage = "./images/mts-short-example.gif";
+        } else if ($(this).hasClass("secondProject")) {
+            //bgImage = "./images/second-project-image.jpg";
+        } else if ($(this).hasClass("thirdProject")) {
+            //bgImage = "./images/third-project-image.jpg";
         }
+
+        $(this).closest(".card-cover").css({
+            "background-image": `url('${bgImage}')`,
+            "background-size": "cover",
+            "background-position": "center"
+        });
+
+        $(this).css("opacity", "0");
     });
-    $("h3").mouseleave(function() {
-        if ($(this).hasClass("firstProject") || $(this).hasClass("secondProject") || $(this).hasClass("thirdProject")) {
-            $(this).css("color", "white");
-        }
+
+    $("h3").mouseleave(function(evt) {
+        $(this).closest(".card-cover").css({
+            "background-image": "",
+        });
+
+        $(this).css("opacity", "1");
     });
 });
 
