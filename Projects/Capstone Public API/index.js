2df38d11a1a188a5e1eee4df8e092ac87ccd4186
Shakezk <kyleshakes04@gmail.com>
2024-08-26 15:28:02 +0100

    Updated API Capstone Code

--- Projects/Capstone Public API/index.js
+++ Projects/Capstone Public API/index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 
 const apiKey = "yourCoinGeckoAPIKey";
-const apiLink = "https://api.coingecko.com/api/v3/";
+const apiLink = "https://api.coingecko.com/api/v3/coins/markets";
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -14,22 +14,25 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 const config = {
     headers: {"x-cg-demo-api-key": apiKey},
+    "Cache-Control": "no-cache",
+    accept: 'application/json'
 }
 
 app.listen(port, () => {
     console.log(`Listening on localhost:${port}`);
 });
 
-let jsonData;
-
 app.get("/", async (req, res) => {
     try{
-        const response = await axios.get(apiLink + 'coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=10&price_change_percentage=24h%2C7d%2C30d', config);
+        const response = await axios.get(apiLink + '?vs_currency=gbp&order=market_cap_desc&per_page=10&price_change_percentage=24h%2C7d%2C30d', config);
+        const jsonData = response.data;
+        res.render("index.ejs", {data: jsonData});
 
-        jsonData = response.data;
     } catch (error){
         console.error(`Error message: ${error.message}`);
+
+        res.render("index.ejs", {data: []});
     }
-    res.render("index.ejs", {data: jsonData});
+    
 });
 
