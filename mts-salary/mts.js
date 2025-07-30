1a7c616cc685085d31c9c9ac1a4835c7e0b9df51
Kyle <kyleshakes04@gmail.com>
2025-07-30 12:57:38 +0100

    Updated mts.js Added comments Made new function showUsersPay() to separate the script into sections rather than having it all in 1 conditional statement. Added "has-overtime" button so there are no longer potentially unnecessary fields showed.

--- mts-salary/mts.js
+++ mts-salary/mts.js
@@ -3,8 +3,16 @@ and salaries that dont fall into a tax bracket */
 
 var allowance = 1047.50;
 var nonNumericRegex = /[^0-9.]/;
+
+var hasOvertime = false;
+if (!hasOvertime){
+  $('.overtime-container').hide();
+}
+
 $(".calculate-salary").click(function(){
-  var rate = $("#hourly-rate-text").val();
+  // When the calculate-salary button is clicked, grab the text or the placeholder
+  var rate = $("#hourly-rate-text").val() || $("#hourly-rate-text").attr("placeholder");
+  // If the user input is ont appropriate to process, 
   if (nonNumericRegex.test(rate) ||  $.trim(rate) === '' || $.trim(rate) === "0") {
     console.log("Hourly Rate Not Given");
     $('body').toggleClass("shake");
@@ -12,19 +20,44 @@ $(".calculate-salary").click(function(){
       $('body').toggleClass("shake");
     }, 500);
 
+    // If the users input is an appropriate input, process it
   }else{
     rate = parseFloat(rate);
+    // Grab all the OT pay values and calculate the total pay
     var fridayOTPay = $("#fridayOT-worked-text").val() * (rate * 1.33);
     var saturdayOTPay = $("#saturdayOT-worked-text").val() * (rate * 1.50);
     var sundayOTPay = $("#sundayOT-worked-text").val() * (rate * 2);
     var extraPay = parseFloat($("#extra-pay-text").val()) || 0;
+    var hoursWorkedPerDay = 8;
 
-    var preTaxPay = ((260/12) * (rate) * 9) + fridayOTPay + saturdayOTPay + sundayOTPay + extraPay;
+    var preTaxPay = ((260/12) * (rate) * hoursWorkedPerDay) + fridayOTPay + saturdayOTPay + sundayOTPay + extraPay;
     var tax = (preTaxPay - allowance) * 0.20;
     var nationalInsurance = (preTaxPay - allowance) * 0.08;
     var postTax = preTaxPay - tax - nationalInsurance;
+    showUsersPay(preTaxPay, tax, nationalInsurance, postTax);
+    
+  }  
+})
 
-    $('.show-salary-container').css('display', 'flex');
+function animateValue(obj, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    let value = progress * (end - start) + start;
+
+    if (value !== Math.floor(value)){
+      obj.innerHTML = "£" + (Math.round(value * 100) / 100).toFixed(2);
+    }
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+function showUsersPay(preTaxPay, tax, nationalInsurance, postTax){
+  $('.show-salary-container').css('display', 'flex');
     const preTaxPayObj = document.getElementById("preTaxPay");
     const nIDeductObj = document.getElementById("nIDeduct");
     const taxDeductObj = document.getElementById("taxDeduct");
@@ -50,27 +83,17 @@ $(".calculate-salary").click(function(){
     animateValue(nIDeductObj, 0, nationalInsurance, 2000);
     animateValue(taxDeductObj, 0, tax, 2000);
     animateValue(totalPayObj, 0, postTax, 2000);
-  }  
-})
-
-function animateValue(obj, start, end, duration) {
-  let startTimestamp = null;
-  const step = (timestamp) => {
-    if (!startTimestamp) startTimestamp = timestamp;
-    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-    let value = progress * (end - start) + start;
-
-    if (value !== Math.floor(value)){
-      obj.innerHTML = "£" + (Math.round(value * 100) / 100).toFixed(2);
-    }
-    if (progress < 1) {
-      window.requestAnimationFrame(step);
-    }
-  };
-  window.requestAnimationFrame(step);
 }
 
 $(".backButton").click(function(){
   $('.show-salary-container').fadeOut("slow"); // make it disappear
+});
+
+$('#has-overtime').click(function(){
+  hasOvertime = !hasOvertime;
+  if (hasOvertime === true){
+    $('.overtime-container').css('display', 'flex');
+    $('#has-overtime').hide();
+  }
 });
 
